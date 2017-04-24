7d14ac101a89b6aebb0105d491d08cb8f828bc01
dishaumarwani <dhu200@nyu.edu>
2017-04-24 19:36:30 -0400

    before-adding-material-design

--- Restaurant_Recommendation_System/urls.py
+++ Restaurant_Recommendation_System/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from restaurant.views import index, detail
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^restaurant/', include('restaurant.urls')),
+
 ]
 
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -2,11 +2,14 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from . import views
+from django.contrib.auth.views import login
+from django.contrib.auth import views as auth_views
+from restaurant.views import index, detail
 
 app_name = 'restaurant'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<Restaurant_id>[0-9]+)/$', views.detail,name='detail'),
-    url(r'^CustomerRegister/$', views.CustomerRegister, name='CustomerRegister')
+    url(r'^CustomerRegister/$', views.CustomerRegister, name='CustomerRegister'),
     ]
